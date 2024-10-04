681b7fe76d1e2bc29a9c4952259cfce102de7db7
spherinder <spherinder@gmail.com>
2024-10-04 18:22:44 +0200

    repl setup

--- flake.nix
+++ flake.nix
@@ -15,11 +15,15 @@
             ocaml-ng.ocamlPackages_4_13.ocaml
             ocaml-ng.ocamlPackages_4_13.ocamlbuild
             ocaml-ng.ocamlPackages_4_13.menhir
-            # ocaml-ng.ocamlPackages_4_13.merlin
-            ocamlPackages.merlin
-            ocamlPackages.ocp-indent
-            ocamlPackages.ocamlformat
-            opam
+            ocaml-ng.ocamlPackages_4_13.num
+            ocaml-ng.ocamlPackages_4_13.findlib
+            ocaml-ng.ocamlPackages_4_13.merlin
+            ocaml-ng.ocamlPackages_4_13.ocp-indent
+            ocaml-ng.ocamlPackages_4_13.ocamlformat
+            # ocamlPackages.merlin
+            # ocamlPackages.ocp-indent
+            # ocamlPackages.ocamlformat
+            # opam
           ];
         };
       });
